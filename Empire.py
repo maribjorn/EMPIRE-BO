0b8752170abe4c8fbff026b450d0e40c42858650
maribjorn <115071899+maribjorn@users.noreply.github.com>
2024-04-29 08:11:24 +0200

    battery constraints

--- Empire.py
+++ Empire.py
@@ -771,8 +771,63 @@ def run_empire(name, tab_file_path, result_file_path, scenariogeneration, scenar
 
     # Add the constraint to the model
     model.NewBuiltCapacityLimit = Constraint(model.PeriodActive, rule=new_built_capacity_limit_rule)
+    
+    #################################################################
+    
+    #ADDING CONSTRAINTS FOR TOTAL CAPACITY LIMITS IN THE REST OF THE PERIODS
+    def new_built_capacity_limit_rule2(model, i):
+        if i != 1:
+            return sum(model.genInvCap[n, g, i] for (n, g) in model.GeneratorsOfNode) - 2000 <= 0
+        else:
+            return Constraint.Skip
+        
+    model.NewBuiltCapacityLimit2 = Constraint(model.PeriodActive, rule=new_built_capacity_limit_rule2)
+    
+    #################################################################
+    
+    # ADDING THE SAME CONSTRAINT FOR THE ENERGY STORAGE CAPACITY LIMIT FOR PERIOD 1
+    def new_built_capacity_energy_limit_rule1(model, i):
+        if i == 1:
+            return sum(model.storENInvCap[n, b, i] for (n, b) in model.StoragesOfNode) - 800 <= 0
+        else:
+            return Constraint.Skip
+        
+    model.NewBuiltCapacityEnergyLimit1 = Constraint(model.PeriodActive, rule=new_built_capacity_energy_limit_rule1)
+    
 
+    #################################################################
+    
+    # ADDING THE SAME CONSTRAINT FOR THE ENERGY STORAGE CAPACITY LIMIT FOR THE REST OF THE PERIODS
+    def new_built_capacity_energy_limit_rule2(model, i):
+        if i != 1:
+            return sum(model.storENInvCap[n, b, i] for (n, b) in model.StoragesOfNode) - 800 <= 0
+        else:
+            return Constraint.Skip
+        
+    model.NewBuiltCapacityEnergyLimit2 = Constraint(model.PeriodActive, rule=new_built_capacity_energy_limit_rule2)
+    
+    #################################################################
+    
+    # ADDING THE SAME CONSTRAINT FOR THE ENERGY STORAGE CAPACITY LIMIT FOR PERIOD 1
+    def new_built_capacity_battery_limit_rule1(model, i):
+        if i == 1:
+            return sum(model.storPWInvCap[n, b, i] for (n, b) in model.StoragesOfNode) - 400 <= 0
+        else:
+            return Constraint.Skip
+        
+    model.NewBuiltCapacityBatteryLimit1 = Constraint(model.PeriodActive, rule=new_built_capacity_battery_limit_rule1)
+    
 
+    #################################################################
+    
+    # ADDING THE SAME CONSTRAINT FOR THE ENERGY STORAGE CAPACITY LIMIT FOR THE REST OF THE PERIODS
+    def new_built_capacity_battery_limit_rule2(model, i):
+        if i != 1:
+            return sum(model.storPWInvCap[n, b, i] for (n, b) in model.StoragesOfNode) - 400 <= 0
+        else:
+            return Constraint.Skip
+        
+    model.NewBuiltCapacityBatteryLimit2 = Constraint(model.PeriodActive, rule=new_built_capacity_battery_limit_rule2)
     
     #################################################################
 
